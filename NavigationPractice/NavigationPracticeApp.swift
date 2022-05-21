da1466c3c56506523e2c20a803fc426586e08d50
David Argote <44904958+argote-dev@users.noreply.github.com>
2022-05-20 21:02:17 -0500

    Initial Commit

---
+++ NavigationPractice/NavigationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPracticeApp.swift
+//  NavigationPractice
+//
+//  Created by David Argote on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
